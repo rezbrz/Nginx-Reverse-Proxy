0c42bcb2a46e481b333025ffd754b9995f8eae96
rezbrz <102940947+rezbrz@users.noreply.github.com>
2022-04-04 12:05:41 +0430

    Add files via upload

---
+++ flask app/run.py
@@ -0,0 +1,42 @@
+from flask import Flask 
+from flask_restful import Resource, Api, reqparse
+#import pandas as pd
+import ast
+
+app = Flask(__name__)
+api = Api(app)
+
+class Users(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()  # initialize
+        parser.add_argument('userId', required=True)  # add args
+        args = parser.parse_args()
+        
+        dict = {'a1b': 'Joe', 'a2c': 'Jenny', 'b1b': 'Jack'}
+        
+        if args['userId'] in dict.keys():
+            return {'data': dict[args['userId']]}, 200  # return data and 200 OK
+        else:
+            return {'message': f"'{args['userId']}' does not exists."}, 409
+        
+        
+        
+
+                    
+class Locations(Resource):
+    def get(self,locationId):     
+        
+        dict = {'1': 'Cafe Florance', '2': 'Cafe Tabac', '3': 'Rosslyn Coffee,'}
+        
+        if locationId in dict.keys():
+            return {'data': dict[locationId]}, 200  # return data and 200 OK
+        else:
+            return {'message': f"'{locationId}' does not exists."}, 409
+
+    
+
+api.add_resource(Users, '/users')  # add endpoints
+api.add_resource(Locations, '/locations/<locationId>')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port='5002')  # run our Flask app
